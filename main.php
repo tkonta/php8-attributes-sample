57c765fde24467d5b6cbb54b62633193bfb4362e
tkonta <saihateno@gmail.com>
2022-12-09 04:36:44 +0900

    change main

--- main.php
+++ main.php
@@ -6,19 +6,20 @@ use tkonta\Php8AttributesSample\Introduce;
 
 require __DIR__ . '/vendor/autoload.php';
 
-function main(string $context)
+function main()
 {
     $family = new Family('山田父', '山田息子');
     $introduce = new Introduce($family);
-    $introduce->talk($context);
-}
 
-if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
     print_r("context: 自己紹介\n");
-    main('自己紹介');
+    $introduce->talk("自己紹介");
 
     print_r("------\n");
 
     print_r("context: 家族紹介\n");
-    main('家族紹介');
+    $introduce->talk("家族紹介");
+}
+
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+    main();
 }
